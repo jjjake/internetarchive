15453d3f374ac48d212fd89311e151f72546e73f
Jake Johnson <jake@archive.org>
2012-08-29 21:47:54 +0000

    Added Catalog Class

--- archive.py
+++ archive.py
@@ -1,7 +1,3 @@
-__author__ = "Jake Johnson (jake@archive.org)"
-__version__ = "0.1"
-__license__ = "Public Domain"
-
 import logging
 import time
 import os
@@ -9,8 +5,10 @@ import json
 
 import requests
 from lxml import etree
+
 from jsonklog.formatters import JSONFormatter
 from jsonklog.formatters import JSONFormatterSimple
+
 import boto
 from boto.s3.key import Key
 from boto.s3.connection import OrdinaryCallingFormat
@@ -31,6 +29,8 @@ COOKIES = {'logged-in-sig': os.environ['LOGGED_IN_SIG'],
 
 S3_CONNECTION = boto.connect_s3(host='s3.us.archive.org',
                                 calling_format=OrdinaryCallingFormat())
+
+
 
 class User(object):
 
@@ -62,10 +62,22 @@ class Item(object):
         self.metadata_url = 'http://archive.org/metadata/%s' % identifier
         self.req = requests.get(self.metadata_url)
         self.metadata = json.loads(self.req.text)
+        if self.metadata == {}:
+            self.exists = False
+        else:
+            self.exists = True
 
-    # S3 Uploader >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    #__________________________________________________________________________
-    def get_bucket(self, headers):
+
+    # METADATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+    def write_metadata(self, patch, target="metadata"):
+        params = json.dumps({"-patch": PATCH, "-target": target})
+        url = 'http://archive.org/metadata/%s' % item
+        r = requests.patch(self.metadata_url, params=params, cookies=COOKIES)
+        return r.text
+
+
+    # UPLOADING ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
+    def get_s3_bucket(self, headers={}, make_bucket=False):
         log.debug('Getting Bucket...')
         bucket = S3_CONNECTION.lookup(self.identifier)
 
@@ -73,18 +85,20 @@ class Item(object):
             log.debug('Found existing bucket %s' % self.identifier)
             return bucket
 
-        log.debug('Creating new bucket %s' % self.identifier)
-        headers['x-archive-queue-derive'] = 0
-        bucket = S3_CONNECTION.create_bucket(self.identifier, headers=headers)
-        i=0
-        while i<60:
-            b = S3_CONNECTION.lookup(self.identifier)
-            if b:
-                return bucket
-            log.debug('Waiting for bucket creation...')
-            time.sleep(10)
-            i+=1
-        raise NameError('Could not create or lookup ' + self.identifier)
+        if make_bucket:
+            log.debug('Creating new bucket %s' % self.identifier)
+            headers['x-archive-queue-derive'] = 0
+            bucket = S3_CONNECTION.create_bucket(self.identifier,
+                                                 headers=headers)
+            i=0
+            while i<60:
+                b = S3_CONNECTION.lookup(self.identifier)
+                if b:
+                    return bucket
+                log.debug('Waiting for bucket creation...')
+                time.sleep(10)
+                i+=1
+            raise NameError('Could not create or lookup %s' % self.identifier)
 
     def upload(self, files, meta_dict):
 
@@ -103,7 +117,7 @@ class Item(object):
             files = [files]
         for file in files:
             filename = file.split('/')[-1]
-            bucket = self.get_bucket(headers)
+            bucket = self.get_s3_bucket(headers, make_bucket=True)
             if bucket.get_key(filename):
                 log.warning('File already exists, not deleting from server!')
                 return None
@@ -111,3 +125,32 @@ class Item(object):
             k.name = filename
             k.set_contents_from_filename(file)
             log.info('Created: http://archive.org/details/%s' % self.identifier)
+
+
+
+class Catalog(object):
+
+    def __init__(self, catalog_url):
+        url = '%s&json=2&output=json&callback=foo' % catalog_url
+        r = requests.get(url, cookies=COOKIES)
+
+        # This ugly little line is used to parse the faux JSON available from
+        # catalog.php
+        self.tasks_json = json.loads(r.text.strip('foo').strip().strip('()'))
+        self.tasks = []
+        for t in self.tasks_json:
+            td = {}
+            td['identifier'] = t[0]
+            td['server'] = t[1]
+            td['command'] = t[2]
+            td['time'] = t[3]
+            td['submitter'] = t[4]
+            td['args'] = t[5]
+            td['task_id'] = t[6]
+            td['type'] = t[7]
+            self.tasks.append(td)
+
+        self.green_rows = [x for x in self.tasks if x['type'] == 0]
+        self.blue_rows = [x for x in self.tasks if x['type'] == 1]
+        self.red_rows = [x for x in self.tasks if x['type'] == 2]
+        self.brown_rows = [x for x in self.tasks if x['type'] == 9]

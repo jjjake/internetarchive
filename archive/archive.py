d13e7c3adfb6d8f6eb5a91223d25ef11f1e9799e
Jake Johnson <jake@archive.org>
2012-09-07 22:26:46 +0000

    Removed User class, and moved logging, etc. to archive/__init__.py

--- archive/archive.py
+++ archive/archive.py
@@ -1,55 +1,13 @@
-import logging
 import time
 import os
 import json
 
 import requests
 from lxml import etree
-
-from jsonklog.formatters import JSONFormatter
-from jsonklog.formatters import JSONFormatterSimple
-
-import boto
 from boto.s3.key import Key
-from boto.s3.connection import OrdinaryCallingFormat
+from jsonpatch import make_patch
 
-LOG_FILE = '%s-archive.py.log' % time.strftime("%Y-%m-%d", time.gmtime())
-LOG_LEVEL = logging.DEBUG
-
-log = logging.getLogger(__name__)
-log.setLevel(LOG_LEVEL)
-handler_full = logging.StreamHandler(open(LOG_FILE, 'ab'))
-handler_full.setFormatter(JSONFormatter())
-logging.Formatter.converter = time.gmtime
-log.addHandler(handler_full)
-
-COOKIES = {'logged-in-sig': os.environ['LOGGED_IN_SIG'],
-           'logged-in-user': os.environ['LOGGED_IN_USER'],
-}
-
-S3_CONNECTION = boto.connect_s3(host='s3.us.archive.org',
-                                calling_format=OrdinaryCallingFormat())
-
-
-
-class User(object):
-
-    def __init__(self, user):
-        self.user = user
-
-    def tasks(self, task_type=None):
-        tasks_url = ('http://archive.org/catalog_status.php?'
-                     'where=submitter="%s"' % self.user)
-        xml_object = etree.parse(tasks_url)
-        raw_tasks = {'greenrows': xml_object.find('wait_admin0'),
-                     'bluerows': xml_object.find('wait_admin1'),
-                     'redrows': xml_object.find('wait_admin2'),
-        }
-        tasks = {k: v.text for k,v in raw_tasks.iteritems() if v is not None}
-        if task_type:
-            return tasks.get(task_type)
-        else:
-            return tasks
+from config import DotDict, log_in_cookies, log, s3_connection
 
 
 
@@ -61,7 +19,7 @@ class Item(object):
         self.download_url = 'http://archive.org/download/%s' % identifier
         self.metadata_url = 'http://archive.org/metadata/%s' % identifier
         self.req = requests.get(self.metadata_url)
-        self.metadata = json.loads(self.req.text)
+        self.metadata = json.loads(self.req.text, object_hook=DotDict)
         if self.metadata == {}:
             self.exists = False
         else:
@@ -70,16 +28,21 @@ class Item(object):
 
     # METADATA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
     def write_metadata(self, patch, target="metadata"):
-        params = json.dumps({"-patch": PATCH, "-target": target})
-        url = 'http://archive.org/metadata/%s' % item
-        r = requests.patch(self.metadata_url, params=params, cookies=COOKIES)
-        return r.text
+        src = self.metadata[target]
+        dest = dict((src.items() + patch.items()))
+        patch = make_patch(src, dest) #.patch
+        print patch
+        print dir(patch)
+        print dir(patch.patch)
+        params = {"-patch": json.dumps(patch), "-target": target}
+        r = requests.patch(self.metadata_url, params=ptextarams, cookies=log_in_cookies)
+        return r
 
 
     # UPLOADING ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~>
     def get_s3_bucket(self, headers={}, make_bucket=False):
         log.debug('Getting Bucket...')
-        bucket = S3_CONNECTION.lookup(self.identifier)
+        bucket = s3_connection.lookup(self.identifier)
 
         if bucket is not None:
             log.debug('Found existing bucket %s' % self.identifier)
@@ -88,11 +51,11 @@ class Item(object):
         if make_bucket:
             log.debug('Creating new bucket %s' % self.identifier)
             headers['x-archive-queue-derive'] = 0
-            bucket = S3_CONNECTION.create_bucket(self.identifier,
+            bucket = s3_connection.create_bucket(self.identifier,
                                                  headers=headers)
             i=0
             while i<60:
-                b = S3_CONNECTION.lookup(self.identifier)
+                b = s3_connection.lookup(self.identifier)
                 if b:
                     return bucket
                 log.debug('Waiting for bucket creation...')
@@ -100,7 +63,7 @@ class Item(object):
                 i+=1
             raise NameError('Could not create or lookup %s' % self.identifier)
 
-    def upload(self, files, meta_dict):
+    def upload(self, files, meta_dict, dry_run=False, derive=True):
 
         headers = {'x-archive-meta-%s' % k: v for k,v in
                    meta_dict.iteritems() if type(v) != list}
@@ -113,6 +76,9 @@ class Item(object):
                     i+=1
         headers = {k: v for k,v in headers.iteritems() if v}
 
+        if dry_run:
+            return headers
+
         if not type(files) == list:
             files = [files]
         for file in files:
@@ -121,18 +87,24 @@ class Item(object):
             if bucket.get_key(filename):
                 log.warning('File already exists, not deleting from server!')
                 return None
+            if not derive:
+                headers = {'x-archive-queue-derive': 0}
             k = Key(bucket)
             k.name = filename
-            k.set_contents_from_filename(file)
+            k.set_contents_from_filename(file, headers=headers)
             log.info('Created: http://archive.org/details/%s' % self.identifier)
 
 
 
 class Catalog(object):
 
-    def __init__(self, catalog_url):
-        url = '%s&json=2&output=json&callback=foo' % catalog_url
-        r = requests.get(url, cookies=COOKIES)
+    def __init__(self, params=None):
+        if not params:
+            params = {'justme': 1, 'json': 2, 'output': 'json',
+                      'callback': 'foo',
+            }
+        url = 'http://www.us.archive.org/catalog.php'
+        r = requests.get(url, params=params, cookies=log_in_cookies)
 
         # This ugly little line is used to parse the faux JSON available from
         # catalog.php

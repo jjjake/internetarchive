4ac076515358b379af153f49df757615d0cb0af3
Jake Johnson <jake@archive.org>
2013-08-07 21:31:20 +0000

    changed date

--- archive/__init__.py
+++ archive/__init__.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 
 """
-archive
-~~~~~~~
+Internet Archive library
+~~~~~~~~~~~~~~~~~~~~~~~~
 
-:copyright: (c) 2012 by Jacob M. Johnson.
+Archive is an Internet Archive lbirary, written in python. 
+
+:copyright: (c) 2013 by Jacob M. Johnson.
 :license: GPL, see LICENSE for more details.
 
 """
@@ -13,6 +15,6 @@ __title__ = 'archive'
 __version__ = '0.1.0'
 __author__ = 'Jacob M. Johnson'
 __license__ = 'GPL'
-__copyright__ = 'Copyright 2012 Jacob M. Johnson'
+__copyright__ = 'Copyright 2013 Jacob M. Johnson'
 
 from .archive import Item, Catalog

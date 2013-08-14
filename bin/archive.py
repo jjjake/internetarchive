529950e21c354f43f40bdae24e9e341472c78ac1
Jake Johnson <jake@archive.org>
2013-08-14 23:20:46 +0000

    Added Metadata and Search sub-commands.

--- bin/archive.py
+++ bin/archive.py
@@ -6,6 +6,34 @@ import internetarchive
 
 
 
+# metadata()
+#_________________________________________________________________________________________
+def metadata(args):
+    if not args.identifier:
+        sys.stderr.write('error: no identifier!\n')
+        sys.exit(1)
+    item = internetarchive.Item(args.identifier)
+    if args.files_count:
+        sys.stdout.write(str(item.metadata.get('files_count')))
+    elif args.server:
+        sys.stdout.write(str(item.metadata.get('server')))
+    elif args.dir:
+        sys.stdout.write(str(item.metadata.get('dir')))
+    elif args.d1:
+        sys.stdout.write(str(item.metadata.get('d1')))
+    elif args.d2:
+        sys.stdout.write(str(item.metadata.get('d2')))
+    elif args.files:
+        for f in item.files():
+            files_md = [f.item.identifier, f.name, f.format, f.size, f.md5]
+            sys.stdout.write('\t'.join([str(x) for x in files_md]) + '\n')
+    elif args.item_size:
+        sys.stdout.write(str(item.metadata.get('item_size')))
+    else:
+        sys.stdout.write(str(item.metadata))
+    sys.exit(0)
+
+
 # upload()
 #_________________________________________________________________________________________
 def upload(args):
@@ -15,7 +43,6 @@ def upload(args):
     if not args.files:
         sys.stderr.write('error: no files!\n')
         sys.exit(1)
-
     headers = dict((h.split(':') for h in args.header))
     metadata = dict((md.split('=') for md in args.metadata))
     item = internetarchive.Item(args.identifier[0])
@@ -25,16 +52,26 @@ def upload(args):
                                 ignore_bucket=args.ignore_bucket)
     if args.dry_run:
         sys.stdout.write('IA-S3 Headers:\n\n{0}\n'.format(upload_status))
+        sys.exit(0)
     elif upload_status is False:
         sys.stderr.write('error: upload failed!\n')
+        sys.exit(1)
     else:
         sys.stdout.write('uploaded:\t{0}\n'.format(item.details_url))
+        sys.exit(0)
 
 
-# metadata()
+# search()
 #_________________________________________________________________________________________
-def metadata():
-    pass
+def search(args):
+    if not args.query:
+        sys.stderr.write('error: no query!\n')
+        sys.exit(1)
+    query = ' '.join(args.query)
+    search = internetarchive.Search(query)
+    for result in search.results:
+        sys.stdout.write(result['identifier'] + '\n')
+    sys.exit(0)
 
 
 # main()
@@ -43,10 +80,22 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='archive.py')
     subparsers = parser.add_subparsers(help='sub-command help')
 
+    # Metadata parser.
+    parser_metadata = subparsers.add_parser('metadata', help='metadata help')
+    parser_metadata.add_argument('identifier', nargs='?', type=str)
+    parser_metadata.add_argument('--files_count', '-fc', default=False,
+                                 action='store_true')
+    parser_metadata.add_argument('--files', default=False, action='store_true')
+    parser_metadata.add_argument('--server', default=False, action='store_true')
+    parser_metadata.add_argument('--dir', default=False, action='store_true')
+    parser_metadata.add_argument('--d1', default=False, action='store_true')
+    parser_metadata.add_argument('--d2', default=False, action='store_true')
+    parser_metadata.add_argument('--item_size', default=False, action='store_true')
+    parser_metadata.set_defaults(func=metadata)
+
     # Upload parser.
     parser_upload = subparsers.add_parser('upload', help='upload help')
     parser_upload.add_argument('files', nargs='*', type=str, default=None)
-    parser_upload.add_argument('--identifier', '-id', type=str, action='append')
     parser_upload.add_argument('--derive', default=True, action='store_false')
     parser_upload.add_argument('--multipart', default=False, action='store_true')
     parser_upload.add_argument('--ignore-bucket', default=False, action='store_true')
@@ -55,12 +104,10 @@ if __name__ == '__main__':
     parser_upload.add_argument('--metadata', '-md', type=str, action='append')
     parser_upload.set_defaults(func=upload)
     
-    # Metadata parser.
-    # TODO: Add metadata sub-command for editing and retrieving metadata from Archive.org
-    parser_metadata = subparsers.add_parser('metadata', help='upload help')
-    parser_metadata.add_argument('foo', nargs='*', type=str, default=None)
-    parser_metadata.set_defaults(func=metadata)
+    # Search parser.
+    parser_search = subparsers.add_parser('search', help='search help')
+    parser_search.add_argument('query', nargs='*', type=str)
+    parser_search.set_defaults(func=search)
 
-    # Args.
     args = parser.parse_args()
     args.func(args)

2edd119a1143a6f6870e0da229567b13d2b19d1f
Jake Johnson <jake@archive.org>
2013-08-20 21:16:25 +0000

    Automatically install required libraries.

--- setup.py
+++ setup.py
@@ -2,21 +2,14 @@ from setuptools import setup
 
 
 
-requires = [
-    'boto==2.5.2',
-    'jsonpatch==0.4',
-    'filechunkio==1.5',
-]
-
-
 setup(
     name='internetarchive',
     version='0.1',
     author='Jacob M. Johnson',
     author_email='jake@archive.org',
     packages=['internetarchive'],
-    entry_points = dict(
-        console_scripts = [
+    entry_points=dict(
+        console_scripts=[
             'internetarchive = bin.internetarchive_cli:main',
         ]
     ),
@@ -24,4 +17,9 @@ setup(
     license='LICENSE.txt',
     description='A python interface to archive.org.',
     long_description=open('README.md').read(),
+    install_requires=[
+        'boto==2.5.2',
+        'jsonpatch==0.4',
+        'filechunkio==1.5',
+    ]
 )

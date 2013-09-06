97aaabd94267eb372195a7421b5ac406fbe00fe1
Jake Johnson <jake@archive.org>
2013-09-05 21:51:00 -0700

    Mergeing docopt and concurrent download branches...

--- internetarchive/internetarchive.py
+++ internetarchive/internetarchive.py
@@ -289,7 +289,8 @@ class Item(object):
         """
 
         headers = self._get_s3_headers(headers, metadata)
-        if 'x-archive-size-hint' not in [ header_name.lower() for header_name in headers.keys()]:
+        header_names = [header_name.lower() for header_name in headers.keys()]
+        if 'x-archive-size-hint' not in header_names:
             headers['x-archive-size-hint'] = os.stat(_file).st_size
         scanner = 'Internet Archive Python library {0}'.format(__version__)
         headers['x-archive-meta-scanner'] = scanner

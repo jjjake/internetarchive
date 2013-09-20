12744b7d58cc04676539058b56d7b87c17127312
Jake Johnson <jake@archive.org>
2013-09-20 23:14:42 +0000

    Added sort option and parameters option (for submitting extra paramters with your request).

--- iacli/ia_search.py
+++ iacli/ia_search.py
@@ -1,7 +1,18 @@
-"""Search archive.org.
+"""Search the Internet Archive using the Archive.org Advanced Search 
+API <https://archive.org/advancedsearch.php#raw>.
 
 usage: 
-    ia search [--help] <query>...
+    ia search [--help] <query>... [options...]
+
+options:
+    -h, --help
+    -p, --parameters=<key:value>...  Parameters to send with your query.
+    -s, --sort=<field:order>...      Sort search results by specified
+                                     fields. <order> can be either "asc"
+                                     for ascending and "desc" for 
+                                     descending.
+    -f, --field=<field>...           Metadata fields to return.
+
 """
 from docopt import docopt
 import sys
@@ -15,8 +26,17 @@ import internetarchive
 def main(argv):
     args = docopt(__doc__, argv=argv)
 
+    params = dict(p.split(':') for p in args['--parameters'])
+    if args['--sort']:
+        for i, field in enumerate(args['--sort']):
+            key = 'sort[{0}]'.format(i)
+            params[key] = field.strip().replace(':', ' ')
+
+    fields = ['identifier'] + args['--field']
+
     query = ' '.join(args['<query>'])
-    search = internetarchive.Search(query)
+    search = internetarchive.Search(query, fields=fields, params=params)
     for result in search.results:
-        sys.stdout.write(result['identifier'] + '\n')
+        output = '\t'.join([result[f] for f in fields]).encode('utf-8')
+        sys.stdout.write(output + '\n')
     sys.exit(0)

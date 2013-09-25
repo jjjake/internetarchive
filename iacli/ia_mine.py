0a0a55cf4bdaedd542ff879ebaad4ee906edb5c6
Jake Johnson <jake@archive.org>
2013-09-25 15:24:01 -0700

    Always save metadata locally.

--- iacli/ia_mine.py
+++ iacli/ia_mine.py
@@ -1,20 +1,20 @@
-"""
+"""Concurrently download metadata for items on Archive.org.
+
 usage:
     ia mine <itemlist.txt> [options...]
 
 options:
     -h, --help  
     -v, --verbose  
-    -f, --files  
-    -w, --workers=<count>  [default: 20]
-    -c, --cache            
+    -w, --workers=<count>  The number requests to run concurrently 
+                           [default: 20].
 
 """
 from docopt import docopt
 import sys
 import json
 
-from internetarchive import Mine
+import internetarchive
 
 
 
@@ -23,14 +23,10 @@ from internetarchive import Mine
 def main(argv):
     args = docopt(__doc__, argv=argv)
 
-    items = [i.strip() for i in open(args['<itemlist.txt>'])]
+    identifiers = [i.strip() for i in open(args['<itemlist.txt>'])]
     workers = int(args.get('--workers', 20)[0])
-    miner = Mine(items, workers=workers)
+    miner = internetarchive.get_data_miner(identifiers, workers=workers)
     for i, item in miner.items():
-        if args['--cache']:
-            sys.stdout.write('saving metadata for: {0}\n'.format(item.identifier))
-            with open('{0}_meta.json'.format(item.identifier), 'w') as fp:
-                json.dump(item.metadata, fp)
-        else:
-            str_metadata = json.dumps(item.metadata)
-            sys.stdout.write(str_metadata)
+        sys.stdout.write('saving metadata for: {0}\n'.format(item.identifier))
+        with open('{0}_meta.json'.format(item.identifier), 'w') as fp:
+            json.dump(item.metadata, fp)

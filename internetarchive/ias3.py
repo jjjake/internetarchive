776ce2f6a7eda8a8dc35943f2f71d339309e8cbb
Jake Johnson <jake@archive.org>
2013-09-26 01:44:52 -0700

    Added test for `internetarchive.ias3.get_headers()`, found bugs, simplified.

--- internetarchive/ias3.py
+++ internetarchive/ias3.py
@@ -1,4 +1,5 @@
 import time
+import json
 
 from boto.s3.connection import S3Connection, OrdinaryCallingFormat
 
@@ -56,35 +57,34 @@ def get_headers(metadata, headers={}):
     :param headers: (optional) S3 compatible HTTP headers.
     """
 
-    def encode_s3_header(header_key, header_value):
-        # because rfc822 http headers disallow _ in names, IA-S3 will
-        # translate two hyphens in a row (--) into an underscore (_).
-        header_key = header_key.replace('_', '--')
-        try:
-            encoded_header_key = header_key.encode('utf-8')
-            encoded_header_value = header_value.encode('utf-8')
-        except AttributeError:
-            encoded_header_key = header_key
-            encoded_header_value = header_value
-        return (encoded_header_key, encoded_header_value)
-
     for meta_key, meta_value in metadata.iteritems():
-        try:
-            # Convert metadata items into multiple header fields if
-            # the item's value is iterable, and append to the
-            # headers dict.
-            if isinstance(meta_value, basestring):
-                meta_value=[meta_value]
-            for i, value in enumerate(meta_value):
-                s3_header_key = 'x-archive-meta{0:02d}-{1}'.format(i, meta_key)
-                encoded_header = encode_s3_header(s3_header_key, value)
-                if encoded_header:
-                    header_key, header_value = encoded_header
-                    headers[header_key] = header_value
-        except TypeError:
-            s3_header_key = 'x-archive-meta-{0}'.format(meta_key)
-            encoded_header = encode_s3_header(s3_header_key, value)
-            if encoded_header:
-                header_key, header_value = encoded_header
-                headers[header_key] = header_value
-    return headers
+
+        # Encode arrays into JSON strings because Archive.org does not 
+        # yet support complex metadata structures in 
+        # <identifier>_meta.xml.
+        if isinstance(meta_value, dict):
+            meta_value = json.dumps(meta_value)
+
+        # Convert the metadata value into a list if it is not already
+        # iterable.
+        if not hasattr(meta_value, '__iter__'):
+                meta_value = [meta_value]
+
+        # Convert metadata items into HTTP headers and add to 
+        # ``headers`` dict.
+        for i, value in enumerate(meta_value):
+            if not value:
+                continue
+            s3_header_key = 'x-archive-meta{0:02d}-{1}'.format(i, meta_key)
+
+            # because rfc822 http headers disallow _ in names, IA-S3 will
+            # translate two hyphens in a row (--) into an underscore (_).
+            s3_header_key = s3_header_key.replace('_', '--')
+            try:
+                if not headers.get(s3_header_key):
+                    headers[s3_header_key] = value.encode('utf-8')
+            except AttributeError:
+                if not headers.get(s3_header_key):
+                    headers[s3_header_key] = value
+
+    return dict((k,v) for k,v in headers.items())
---
+++ tests/test_ias3.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+import os, sys
+inc_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, inc_path)
+
+import internetarchive
+
+def test_get_headers():
+    metadata = {
+            'collection': 'test_collection',
+            'foo': u'தமிழ்',
+            'subject': ['foo', 'bar', 'baz'],
+            'bar': 13,
+            'boo': {'test': 'dict'},
+            'none': None,
+            'none2': False,
+            'test_foo': 'underscore',
+    }
+    headers = {
+            'x-archive-size-hint': 19327352832,
+            'x-archive-test-header': 'test value',
+    }
+
+    ias3_headers = internetarchive.ias3.get_headers(metadata, headers)
+
+    test_output = {
+            # str test.
+            'x-archive-meta00-collection': 'test_collection',
+
+            # unicode test.
+            'x-archive-meta00-foo': '\xe0\xae\xa4\xe0\xae\xae\xe0\xae\xbf\xe0\xae\xb4\xe0\xaf\x8d',
+
+            # int test
+            'x-archive-meta00-bar': 13,
+            
+            # list test.
+            'x-archive-meta00-subject':'foo',
+            'x-archive-meta01-subject': 'bar',
+            'x-archive-meta02-subject': 'baz',
+
+            # convert "_" to "--" test (S3 converts "--" to "_").
+            'x-archive-meta00-test--ddd': 'sdfsdf',
+             
+            # dict test.
+            'x-archive-meta00-boo': '{"test": "dict"}',
+            'x-archive-meta00-test--foo': 'underscore',
+
+            # prepared HTTP headers test.
+            'x-archive-size-hint': 19327352832,
+            'x-archive-test-header': 'test value',
+    }
+
+    for key, value in ias3_headers.items():
+        assert test_output[key] == value

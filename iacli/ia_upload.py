cf33cb728717a074ffa0990cf5104b022013a1bf
Jake Johnson <jake@archive.org>
2013-10-07 22:23:15 -0700

    Added sanity checking to arg before executing.

--- iacli/ia_upload.py
+++ iacli/ia_upload.py
@@ -6,18 +6,18 @@ usage:
     ia upload <identifier> [<file>...|-] [options...]
 
 options:
-
- -h, --help
- -v, --verbose                  Print upload status to stdout.
- -d, --debug                    Print S3 request parameters to stdout and exit. [default: True]
- -r, --remote-name=<name>       When uploading data from stdin, this option sets the remote filename.
- -m, --metadata=<key:value>...  Metadata to add to your item.
- -H, --header=<key:value>...    Valid S3 HTTP headers to send with your request.
- -n, --no-derive                Do not derive uploaded files.
- -M, --multipart                Upload files to archive.org in parts, using multipart.
- -i, --ignore-bucket            Destroy and respecify all metadata. [default: True]
+    -h, --help
+    -v, --verbose                  Print upload status to stdout.
+    -d, --debug                    Print S3 request parameters to stdout and exit. [default: True]
+    -r, --remote-name=<name>       When uploading data from stdin, this option sets the remote filename.
+    -m, --metadata=<key:value>...  Metadata to add to your item.
+    -H, --header=<key:value>...    Valid S3 HTTP headers to send with your request.
+    -n, --no-derive                Do not derive uploaded files.
+    -M, --multipart                Upload files to archive.org in parts, using multipart.
+    -i, --ignore-bucket            Destroy and respecify all metadata. [default: True]
 
 """
+import os
 from sys import stdin, stdout, stderr, exit
 from tempfile import TemporaryFile
 from xml.dom.minidom import parseString
@@ -44,6 +44,18 @@ def get_xml_text(elements, text=''):
 #_________________________________________________________________________________________
 def main(argv):
     args = docopt(__doc__, argv=argv)
+
+    # Make sure command is properly formatted, and files exist.
+    if not args['<file>']:
+        if not os.path.exists(args['<identifier>']):
+            stderr.write('error: missing <file> argument!\n')
+        else:
+            stderr.write('error: missing <identifier> argument!\n')
+        exit(1)
+    for file in args['<file>']:
+        if not os.path.exists(file):
+            stderr.write('error: "{0}" doesn\'t exist!\n'.format(file))
+            exit(1)
 
     if args['--verbose'] and not args['--debug']:
         stdout.write('getting item: {0}\n'.format(args['<identifier>']))

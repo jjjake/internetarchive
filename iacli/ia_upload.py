cffa1b19a25400e64d8ffa6d7ec2feb75e95994b
Jake Johnson <jake@archive.org>
2013-10-08 20:14:56 -0700

    Cleaning up docopt flow.

--- iacli/ia_upload.py
+++ iacli/ia_upload.py
@@ -3,41 +3,35 @@
 IA-S3 Documentation: https://archive.org/help/abouts3.txt
 
 usage: 
-    ia upload <identifier> [<file>...|-] [options...]
+    ia upload [--verbose] [--debug] <identifier> 
+              (<file>... | - --remote-name=<name>)
+              [--metadata=<key:value>...] [--header=<key:value>...]
+              [--no-derive] [--ignore-bucket]
+    ia upload --help
 
 options:
-
- -h, --help
- -v, --verbose                  Print upload status to stdout.
- -d, --debug                    Print S3 request parameters to stdout and exit. [default: True]
- -r, --remote-name=<name>       When uploading data from stdin, this option sets the remote filename.
- -m, --metadata=<key:value>...  Metadata to add to your item.
- -H, --header=<key:value>...    Valid S3 HTTP headers to send with your request.
- -n, --no-derive                Do not derive uploaded files.
- -M, --multipart                Upload files to archive.org in parts, using multipart.
- -i, --ignore-bucket            Destroy and respecify all metadata. [default: True]
+    -h, --help
+    -v, --verbose                  Print upload status to stdout.
+    -d, --debug                    Print S3 request parameters to stdout and 
+                                   exit without sending request.
+    -r, --remote-name=<name>       When uploading data from stdin, this option 
+                                   sets the remote filename.
+    -m, --metadata=<key:value>...  Metadata to add to your item.
+    -H, --header=<key:value>...    S3 HTTP headers to send with your request.
+    -n, --no-derive                Do not derive uploaded files.
+    -i, --ignore-bucket            Destroy and respecify all metadata.
 
 """
 from sys import stdin, stdout, stderr, exit
 from tempfile import TemporaryFile
 from xml.dom.minidom import parseString
+from subprocess import call
 
 from docopt import docopt
 
 from internetarchive import upload
-from iacli.argparser import get_args_dict
+from iacli.argparser import get_args_dict, get_xml_text
 
-
-
-# get_xml_text()
-#_________________________________________________________________________________________
-def get_xml_text(elements, text=''):
-    """:todo: document ``get_xml_text()`` function."""
-    for e in elements:
-        for node in e.childNodes:
-            if node.nodeType == node.TEXT_NODE:
-                text += node.data
-    return text
 
 
 # main()
@@ -48,33 +42,37 @@ def main(argv):
     if args['--verbose'] and not args['--debug']:
         stdout.write('getting item: {0}\n'.format(args['<identifier>']))
 
-    metadata = get_args_dict(args['--metadata'])
-    s3_headers = get_args_dict(args['--header'])
-
     upload_kwargs = dict(
-            metadata=metadata, 
-            headers=s3_headers, 
+            metadata=get_args_dict(args['--metadata']), 
+            headers=get_args_dict(args['--header']), 
             debug=args['--debug'], 
             queue_derive=args['--no-derive'], 
             ignore_bucket=args['--ignore-bucket'],
-            verbose=args['--verbose'],
-    )
+            verbose=args['--verbose'])
 
-    if args['<file>'] == ['-']:
+    # Upload stdin.
+    if args['<file>'] == ['-'] and not args['-']:
+        stderr.write('--remote-name is required when uploading from stdin.\n')
+        call(['ia', 'upload', '--help'])
+        exit(1)
+    if args['-']:
         local_file = TemporaryFile()
         local_file.write(stdin.read())
         local_file.seek(0)
-        upload_kwargs['remote_name'] = args['--remote-name'][0]
+        upload_kwargs['remote_name'] = args['--remote-name']
+    # Upload files.
     else:
         local_file = args['<file>']
 
     response = upload(args['<identifier>'], local_file, **upload_kwargs)
 
     if args['--debug']:
-        for r in response:
+        for i, r in enumerate(response):
+            if i != 0:
+                stdout.write('---\n')
             headers = '\n'.join([' {0}: {1}'.format(k,v) for (k,v) in r.headers.items()])
-            stdout.write('---\n\nEndpoint:\n {0}\n\n'.format(r.url))
-            stdout.write('HTTP Headers:\n{0}\n\n'.format(headers))
+            stdout.write('Endpoint:\n {0}\n\n'.format(r.url))
+            stdout.write('HTTP Headers:\n{0}\n'.format(headers))
     else:
         for resp in response:
             if resp.status_code == 200:

657597b728bfa6d9affe73c7e14383093cfa3613
Jake Johnson <jake@archive.org>
2013-10-08 21:03:22 -0700

    Removed --url and --field options which were not working properly, and possibly unecessary.

--- iacli/ia_catalog.py
+++ iacli/ia_catalog.py
@@ -1,26 +1,23 @@
 """Retrieve information about your catalog tasks.
 
 usage: 
-    ia catalog [--help] [options...]
+    ia catalog [--verbose] [--url=<url>] [--green-rows] [--blue-rows]
+               [--red-rows]
+    ia catalog --help
 
 options:
     -h, --help
-    -v, --verbose       
-    -u, --url=<url>             ...
-    -g, --green-rows            Return information about tasks that have not run.
-    -b, --blue-rows             Return information about running tasks.
-    -r, --red-rows              Return information about tasks that have failed.
-    -f , --fields <field>...    Return only the specified fields. <field> may be one
-                                of the following: 
-                                
-                                identifier, server, command, time, submitter, args, 
-                                task_id, row_type
+    -v, --verbose     Ouptut detailed information for each task.
+    -g, --green-rows  Return information about tasks that have not run.
+    -b, --blue-rows   Return information about running tasks.
+    -r, --red-rows    Return information about tasks that have failed.
+
 """
 from docopt import docopt
-import sys
-import urlparse
+from sys import stdout, exit
+from urlparse import urlparse, parse_qs
 
-import internetarchive
+from internetarchive import Catalog, get_tasks
 
 
 
@@ -29,39 +26,27 @@ import internetarchive
 def main(argv):
     args = docopt(__doc__, argv=argv)
 
-    if args['--url']:
-        parsed_url = urlparse.urlparse(args['--url'][0])
-        params = urlparse.parse_qs(parsed_url.query)
-        c = internetarchive.Catalog(params=params)
-    else:
-        c = internetarchive.Catalog()
     row_types = {
-            c.GREEN: 'green',
-            c.BLUE: 'blue',
-            c.RED: 'red',
-            c.BROWN: 'brown',
+            0: 'green',
+            1: 'blue',
+            2: 'red',
+            9: 'brown',
     }
+
     if args['--green-rows']:
-        tasks = c.green_rows
+        tasks = get_tasks(task_type='green')
     elif args['--blue-rows']:
-        tasks = c.blue_rows
+        tasks = get_tasks(task_type='blue')
     elif args['--red-rows']:
-        tasks = c.red_rows
+        tasks = get_tasks(task_type='red')
     else:
         tasks = c.tasks
     for t in tasks:
-        if args['--fields']:
-            task_info = []
-            for field in args['--fields']:
-                info = eval('t.{0}'.format(field))
-                task_info.append(info)
-        else:
-            task_info = [
-                    t.identifier, t.task_id, t.server, t.time, t.command, 
-                    row_types[t.row_type]
-            ]
-            if args['--verbose']:
-                targs = '\t'.join(['{0}={1}'.format(k, v) for (k,v) in t.args.items()])
-                task_info += [t.submitter, targs]
-        sys.stdout.write('\t'.join([str(x) for x in task_info]) + '\n')
-    sys.exit(0)
+        task_info = [
+            t.identifier, t.task_id, t.server, t.time, t.command, row_types[t.row_type],
+        ]
+        if args['--verbose']:
+            # parse task args and append to task_info list.
+            targs = '\t'.join(['{0}={1}'.format(k, v) for (k,v) in t.args.items()])
+            task_info += [t.submitter, targs]
+        stdout.write('\t'.join([str(x) for x in task_info]) + '\n')

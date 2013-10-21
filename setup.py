77e00cf66fdd4f4b6f29f0a222b53df0f6a173ff
Jake Johnson <jake@archive.org>
2013-10-21 20:20:16 +0000

    Upped version.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if version_info >= (3,):
 
 setup(
     name='internetarchive',
-    version='0.4.3',
+    version='0.4.4',
     author='Jacob M. Johnson',
     author_email='jake@archive.org',
     packages=['internetarchive', 'iacli'],

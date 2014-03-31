b74eca1139bb451b9d1a5ef080b50e89c4cfe133
Yaron Greenwald <fibn144@gmail.com>
2014-03-31 18:14:15 +0000

    Split test_ia_download.py's single test up, and converted it to use py.test's tmpdir.

--- tests/iacli/test_ia_download.py
+++ tests/iacli/test_ia_download.py
@@ -10,7 +10,7 @@ import internetarchive.config
 
 
 
-def test_ia_download():
+def test_ia_download_urls():
     cmd = 'ia download --dry-run nasa'
     proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
     stdout, stderr = proc.communicate()
@@ -24,55 +24,61 @@ def test_ia_download():
     ])
     assert stdout == test_output
 
-    cmd = 'ia download --ignore-existing nasa'
-    proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = proc.communicate()
-    test_output = [
-        'globe_west_540.jpg', 
-        'nasa_archive.torrent', 
-        'nasa_files.xml', 
-        'nasa_meta.xml', 
-        'nasa_reviews.xml', 
-        'NASAarchiveLogo.jpg',
-    ]
-    assert sorted(os.listdir('nasa')) == sorted(test_output)
-    shutil.rmtree('nasa')
 
-    cmd = 'ia download --ignore-existing --glob="*jpg" nasa'
-    proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = proc.communicate()
-    test_output = [
-        'globe_west_540.jpg', 
-        'NASAarchiveLogo.jpg',
-    ]
-    assert sorted(os.listdir('nasa')) == sorted(test_output)
-    shutil.rmtree('nasa')
+def test_ia_download_all(tmpdir):
+    with tmpdir.as_cwd():
+        cmd = 'ia download --ignore-existing nasa'
+        proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = proc.communicate()
+        test_output = [
+            'globe_west_540.jpg', 
+            'nasa_archive.torrent', 
+            'nasa_files.xml', 
+            'nasa_meta.xml', 
+            'nasa_reviews.xml', 
+            'NASAarchiveLogo.jpg',
+        ]
+        assert sorted(os.listdir('nasa')) == sorted(test_output)
 
-    cmd = 'ia download --ignore-existing --source=metadata nasa'
-    proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = proc.communicate()
-    test_output = [
-        'nasa_archive.torrent', 
-        'nasa_files.xml', 
-        'nasa_meta.xml', 
-        'nasa_reviews.xml', 
-    ]
-    assert sorted(os.listdir('nasa')) == sorted(test_output)
-    shutil.rmtree('nasa')
 
-    cmd = 'ia download --ignore-existing --format="Archive BitTorrent" nasa'
-    proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = proc.communicate()
-    test_output = [
-        'nasa_archive.torrent', 
-    ]
-    assert sorted(os.listdir('nasa')) == sorted(test_output)
-    shutil.rmtree('nasa')
+def test_ia_download_glob(tmpdir):
+    with tmpdir.as_cwd():
+        cmd = 'ia download --ignore-existing --glob="*jpg" nasa'
+        proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = proc.communicate()
+        test_output = [
+            'globe_west_540.jpg', 
+            'NASAarchiveLogo.jpg',
+        ]
+        assert sorted(os.listdir('nasa')) == sorted(test_output)
 
-    cmd = 'ia download nasa nasa_meta.xml'
-    proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
-    stdout, stderr = proc.communicate()
-    assert proc.returncode == 0
-    shutil.rmtree('nasa')
-    
-    os.chdir(prevdir)
+def test_ia_download_source(tmpdir):
+    with tmpdir.as_cwd():
+        cmd = 'ia download --ignore-existing --source=metadata nasa'
+        proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = proc.communicate()
+        test_output = [
+            'nasa_archive.torrent', 
+            'nasa_files.xml', 
+            'nasa_meta.xml', 
+            'nasa_reviews.xml', 
+        ]
+        assert sorted(os.listdir('nasa')) == sorted(test_output)
+
+def test_ia_download_format(tmpdir):
+    with tmpdir.as_cwd():
+        cmd = 'ia download --ignore-existing --format="Archive BitTorrent" nasa'
+        proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = proc.communicate()
+        test_output = [
+            'nasa_archive.torrent', 
+        ]
+        assert sorted(os.listdir('nasa')) == sorted(test_output)
+
+def test_ia_download_file(tmpdir):
+    with tmpdir.as_cwd():
+        cmd = 'ia download nasa nasa_meta.xml'
+        proc = Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE)
+        stdout, stderr = proc.communicate()
+        assert proc.returncode == 0
+        assert os.listdir('nasa') == ['nasa_meta.xml']

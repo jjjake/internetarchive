575d10b695287932ac191ccbfdee2be4d2835d15
Yaron Greenwald <fibn144@gmail.com>
2014-03-31 21:58:11 +0000

    Broke test_config.py up into many tests. Used tmpdir when writing files.

--- tests/test_config.py
+++ tests/test_config.py
@@ -6,38 +6,38 @@ import yaml
 
 import internetarchive.config
 
-def test_config():
-    test_conf = {
-        'cookies': {
-            'logged-in-sig': 'test-sig',
-            'logged-in-user': 'test-user',
-        },
-        's3': {
-            'access_key': 'test-access',
-            'secret_key': 'test-secret',
-        }
+test_conf = {
+    'cookies': {
+        'logged-in-sig': 'test-sig',
+        'logged-in-user': 'test-user',
+    },
+    's3': {
+        'access_key': 'test-access',
+        'secret_key': 'test-secret',
     }
+}
 
-    with open('ia_test_conf.yml', 'w') as fp:
+def test_config(tmpdir):
+    test_conf_file = tmpdir.join('ia_test_conf.yml')
+    with test_conf_file.open('w') as fp:
         fp.write(yaml.dump(test_conf, default_flow_style=True))
         
     # Test reading config from a given file.
-    config = internetarchive.config.get_config(config_file='ia_test_conf.yml',
+    config = internetarchive.config.get_config(config_file=str(test_conf_file),
                                                config={'custom': 'test'})
     assert config['cookies']['logged-in-sig'] == 'test-sig'
     assert config['cookies']['logged-in-user'] == 'test-user'
     assert config['s3']['access_key'] == 'test-access'
     assert config['s3']['secret_key'] == 'test-secret'
     assert config['custom'] == 'test'
-    os.remove('ia_test_conf.yml')
 
-    # Test no config.
+def test_no_config():
     os.environ['HOME'] = ''
     #config = internetarchive.config.get_config(config=test_conf)
     config = internetarchive.config.get_config()
     assert config == {}
 
-    # Test custom config.
+def test_custom_config():
     os.environ['HOME'] = ''
     config = internetarchive.config.get_config(config=test_conf)
     assert config['cookies']['logged-in-sig'] == 'test-sig'

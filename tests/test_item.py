8caf89f52e03a77c1a9eba4a325958158e85c897
Yaron Greenwald <fibn144@gmail.com>
2014-03-31 18:12:50 +0000

    Converted test_item.py to use py.test's tmpdir.

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,6 +2,7 @@ import os, sys, shutil
 inc_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.insert(0, inc_path)
 
+
 import internetarchive
 
 def test_item():
@@ -9,23 +10,19 @@ def test_item():
     assert item.metadata['identifier'] == 'nasa'
 
 
-def test_file():
-    item = internetarchive.Item('nasa')
-    filename = 'nasa_meta.xml'
-    file = item.get_file(filename)
-
-    assert not os.path.exists(filename)
-    file.download()
-
-    assert unicode(os.stat(filename).st_size) == file.size
-    os.unlink(filename)
+def test_file(tmpdir):
+    with tmpdir.as_cwd():
+        item = internetarchive.Item('nasa')
+        filename = 'nasa_meta.xml'
+        file = item.get_file(filename)
+        file.download()
+        assert unicode(os.stat(filename).st_size) == file.size
 
 
-def test_download():
-    item = internetarchive.Item('nasa')
-    item_dir = item.identifier
-    assert not os.path.exists(item_dir)
-    item.download()
-    assert os.path.exists(item_dir)
-    assert os.path.exists(os.path.join(item_dir, item.identifier+'_meta.xml'))
-    shutil.rmtree(item_dir)
+def test_download(tmpdir):
+    with tmpdir.as_cwd():
+        item = internetarchive.Item('nasa')
+        item_dir = item.identifier
+        item.download()
+        assert os.path.exists(item_dir)
+        assert os.path.exists(os.path.join(item_dir, item.identifier+'_meta.xml'))

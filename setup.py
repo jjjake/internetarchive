5ad692aa839b4d05ab206e2e0116190ef2d0128f
Jake Johnson <jake@archive.org>
2014-05-12 11:30:24 -0700

    v0.5.9

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import sys
 
 setup(
     name='internetarchive',
-    version='0.5.8',
+    version='0.5.9',
     author='Jacob M. Johnson',
     author_email='jake@archive.org',
     packages=['internetarchive', 'internetarchive.iacli'],

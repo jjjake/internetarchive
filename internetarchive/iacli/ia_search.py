2a4e3ff7fd0f68a7df2deea4783616001a61f1ce
Jake Johnson <jake@archive.org>
2014-12-23 12:05:08 -0800

    Output JSON rather than tab-delimited data.

--- internetarchive/iacli/ia_search.py
+++ internetarchive/iacli/ia_search.py
@@ -2,8 +2,8 @@
 API <https://archive.org/advancedsearch.php#raw>.
 
 usage:
-    ia search [--parameters=<key:value>...] [--sort=<field:order>]
-              [--field=<field>...] [--number-found] [--v2] <query>...
+    ia search [--parameters=<key:value>...] [--sort=<field:order>...]
+              [--itemlist | --field=<field>...] [--number-found] [--v2] <query>...
     ia search --help
 
 options:
@@ -12,12 +12,17 @@ options:
     -s, --sort=<field:order>...      Sort search results by specified fields.
                                      <order> can be either "asc" for ascending
                                      and "desc" for descending.
+    -i, --itemlist                   Output identifiers only.
     -f, --field=<field>...           Metadata fields to return.
     -n, --number-found               Print the number of results to stdout.
     --v2                             Search https://archive.org/v2.
 
 """
 import sys
+try:
+    import ujson as json
+except ImportError:
+    import json
 
 from docopt import docopt
 
@@ -31,23 +36,27 @@ def main(argv):
 
     params = dict(p.split(':') for p in args['--parameters'])
 
+    # format sort paramaters.
     if args['--sort']:
-        if not isinstance(args['--sort'], list):
-            args['--sort'] = [args['--sort']]
         for i, field in enumerate(args['--sort']):
             key = 'sort[{0}]'.format(i)
             params[key] = field.strip().replace(':', ' ')
 
-    fields = ['identifier'] + args['--field']
-
     query = ' '.join(args['<query>'])
-    search_resp = search_items(query, fields=fields, params=params, v2=args['--v2'])
+    if args['--itemlist']:
+        fields = ['identifier']
+    else:
+        fields = args['--field']
+    search = search_items(query, fields=args['--field'], params=params, v2=args['--v2'])
     if args['--number-found']:
-        sys.stdout.write('{0}\n'.format(search_resp.num_found))
+        sys.stdout.write('{0}\n'.format(search.num_found))
         sys.exit(0)
-    for result in search_resp:
-        output = '\t'.join([result.get(f, '') for f in fields]).encode('utf-8')
+    for result in search:
         try:
-            sys.stdout.write(output + '\n')
+            if args['--itemlist']:
+                sys.stdout.write(result.get('identifier', ''))
+            else:
+                json.dump(result, sys.stdout)
+            sys.stdout.write('\n')
         except IOError:
             sys.exit(0)

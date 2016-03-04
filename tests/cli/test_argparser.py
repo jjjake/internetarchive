jjjake/internetarchive

commit b69720b7a67bcda8748891f6545d68c89ba6d8f0

search will not sort by id when page specifier given

internetarchive/cli/argparser.py
@@ -8,6 +8,7 @@ internetarchive.cli.argparser
 """
 from collections import defaultdict
 from xml.dom.minidom import parseString
+import re
 
 
 def get_xml_text(xml_str, tag_name=None, text=None):
@@ -22,19 +23,17 @@ def get_xml_text(xml_str, tag_name=None, text=None):
     return text
 
 
-def get_args_dict(args):
-    # Convert args list into a metadata dict.
-    args = [] if not args else args
-    metadata = defaultdict(list)
-    for md in args:
-        key, value = md.split(':', 1)
-        assert value
-        if value not in metadata[key]:
-            metadata[key].append(value)
-
-    for key in metadata:
+def get_args_dict(strings):
+    args = defaultdict(list)
+    for s in strings:
+        for (key, value) in \
+                [[x.strip() for x in re.split(r'[:=]', p, 1)]
+                    for p in re.split('[,;&]', s)]:
+            assert value
+            if value not in args[key]:
+                args[key].append(value)
+    for key in args:
         # Flatten single item lists.
-        if len(metadata[key]) <= 1:
-            metadata[key] = metadata[key][0]
-
-    return metadata
+        if len(args[key]) <= 1:
+            args[key] = args[key][0]
+    return args

tests/cli/test_argparser.py
@@ -20,16 +20,22 @@ def test_get_xml_text():
 def test_get_args_dict():
     test_input = [
         'collection:test_collection',
-        "description: Attention: multiple colon's",
+        "description: Attention: multiple colons",
         'unicode_test:தமிழ்',
         'subject:subject1',
         'subject:subject2',
+        'a=b&a=c&a=d',
+        'b = a , b = c , b = d',
+        'c = a ; c = b ; c = d'
     ]
     test_output = {
         'collection': 'test_collection',
-        'description': " Attention: multiple colon's",
+        'description': "Attention: multiple colons",
         'unicode_test': 'தமிழ்',
-        'subject': ['subject1', 'subject2']
+        'subject': ['subject1', 'subject2'],
+        'a': ['b', 'c', 'd'],
+        'b': ['a', 'c', 'd'],
+        'c': ['a', 'b', 'd']
     }
     args_dict = get_args_dict(test_input)
     for key, value in args_dict.items():

internetarchive/search.py
@@ -61,7 +61,9 @@ class Search(object):
 
         # Sort by score if no other sort is provided -- if page parameter is
         # not provided.
-        if 'page' not in params and not any(k.startswith('sort') for k, v in params.items()):
+        has_page_param = 'page' in params
+        has_sort_param = any(k.startswith('sort') for k, v in params.items())
+        if not (has_page_param or has_sort_param):
             default_params['sort[0]'] = 'identifier asc'
 
         self.params = default_params.copy()

commit 1d43dc088695af9486c5d8a11066ed1a7fd02026

Fixed arg parsing bug in `ia` CLI.
This has been broken since b69720b7a67bcda8748891f6545d68c89ba6d8f0.
Separating parameters via the CLI no longer supports splitting param
key/val pairs with ',' or ';'. Params must be provided in one of the
following forms:

        ia search -p 'foo=bar&baz=boo'
        ia search -p 'foo:bar' -p  'baz:boo'

internetarchive/cli/argparser.py
@@ -8,7 +8,8 @@ internetarchive.cli.argparser
 """
 from collections import defaultdict
 from xml.dom.minidom import parseString
-import re
+
+from six.moves.urllib.parse import parse_qsl
 
 
 def get_xml_text(xml_str, tag_name=None, text=None):
@@ -23,17 +24,25 @@ def get_xml_text(xml_str, tag_name=None, text=None):
     return text
 
 
-def get_args_dict(strings):
-    args = defaultdict(list)
-    for s in strings:
-        for (key, value) in \
-                [[x.strip() for x in re.split(r'[:=]', p, 1)]
-                    for p in re.split('[,;&]', s)]:
+def get_args_dict(args, query_string=False):
+    args = [] if not args else args
+    metadata = defaultdict(list)
+    for md in args:
+        if query_string:
+            if (':' in md) and ('=' not in md):
+                md = md.replace(':', '=')
+            for key, value in parse_qsl(md):
+                assert value
+                metadata[key] = value
+        else:
+            key, value = md.split(':', 1)
             assert value
-            if value not in args[key]:
-                args[key].append(value)
-    for key in args:
+            if value not in metadata[key]:
+                metadata[key].append(value)
+
+    for key in metadata:
         # Flatten single item lists.
-        if len(args[key]) <= 1:
-            args[key] = args[key][0]
-    return args
+        if len(metadata[key]) <= 1:
+            metadata[key] = metadata[key][0]
+
+    return metadata

internetarchive/cli/ia_search.py
@@ -37,7 +37,7 @@ def main(argv, session=None):
     s = Schema({
         six.text_type: Use(bool),
         '<query>': Use(lambda x: ' '.join(x)),
-        '--parameters': Use(lambda x: get_args_dict(x)),
+        '--parameters': Use(lambda x: get_args_dict(x, query_string=True)),
         '--sort': list,
         '--field': Use(lambda x: ['identifier'] if not x and args['--itemlist'] else x),
     })

internetarchive/cli/ia_tasks.py
@@ -28,7 +28,7 @@ from internetarchive.cli.argparser import get_args_dict
 
 def main(argv, session):
     args = docopt(__doc__, argv=argv)
-    params = get_args_dict(args['--parameter'])
+    params = get_args_dict(args['--parameter'], query_string=True)
 
     row_types = {
         -1: 'done',

tests/cli/test_argparser.py
@@ -22,21 +22,30 @@ def test_get_args_dict():
         'collection:test_collection',
         "description: Attention: multiple colons",
         'unicode_test:தமிழ்',
-        'subject:subject1',
+        'subject:subject1, subject1',
         'subject:subject2',
-        'a=b&a=c&a=d',
-        'b = a , b = c , b = d',
-        'c = a ; c = b ; c = d'
+        'subject:subject3; subject3',
     ]
     test_output = {
         'collection': 'test_collection',
-        'description': "Attention: multiple colons",
+        'description': " Attention: multiple colons",
         'unicode_test': 'தமிழ்',
-        'subject': ['subject1', 'subject2'],
-        'a': ['b', 'c', 'd'],
-        'b': ['a', 'c', 'd'],
-        'c': ['a', 'b', 'd']
+        'subject': ['subject1, subject1', 'subject2', 'subject3; subject3'],
     }
     args_dict = get_args_dict(test_input)
+    for key, value in args_dict.items():
+        print(key, value)
+        assert test_output[key] == value
+
+
+def test_get_args_dict_query_string():
+    test_input = ['a=b,foo&c=d&e=f', 'foo:bar ']
+    test_output = {
+        'a': 'b,foo',
+        'c': 'd',
+        'e': 'f',
+        'foo': 'bar ',
+    }
+    args_dict = get_args_dict(test_input, query_string=True)
     for key, value in args_dict.items():
         assert test_output[key] == value
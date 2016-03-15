50d23630a1bcfdc6cdcc626acd0f08c5300e0131
jake <jake@archive.org>
2016-03-15 16:31:54 -0700

    Updated requests lib version requirements.
    Resolve regression introduced in 2.9.0 of the requests lib that made
    it impossible to send binary strings as bodies in Python 3. The
    internetarchive lib now requires versions 2.9.1 - 3.0.0.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
         ],
     },
     install_requires=[
-        'requests>=2.9.0,<3.0.0',
+        'requests>=2.9.1,<3.0.0',
         'jsonpatch==0.4',
         'docopt>=0.6.0,<0.7.0',
         'clint>=0.4.0,<0.6.0',

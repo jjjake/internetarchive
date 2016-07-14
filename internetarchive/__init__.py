ffd343baa99784aedb3141fc86acb65c0e4960d7
jake <jake@archive.org>
2016-07-14 16:18:17 -0700

    Accidently imported non-existing function.

--- internetarchive/__init__.py
+++ internetarchive/__init__.py
@@ -48,8 +48,7 @@ from internetarchive.search import Search
 from internetarchive.catalog import Catalog
 from internetarchive.session import ArchiveSession
 from internetarchive.api import get_item, get_files, modify_metadata, upload, \
-    download, delete, get_tasks, search_items, get_session, configure, get_username, \
-    get_user_info
+    download, delete, get_tasks, search_items, get_session, configure, get_username
 
 
 __all__ = [
@@ -74,7 +73,6 @@ __all__ = [
     'get_session',
     'configure',
     'get_username',
-    'get_user_info',
 ]
 
 

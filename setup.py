f4a275e4efa760cd66ebeecd8564703c9226488c
jake <jake@archive.org>
2017-07-07 15:19:09 -0700

    Loosen up jsonpatch version requirements.
    Previously the Metadata API only supported an early version of the JSON
    Patch standard, so we were locked in to an early version of jsonpatch.
    This is no longer the case.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     },
     install_requires=[
         'requests>=2.9.1,<3.0.0',
-        'jsonpatch==0.4',
+        'jsonpatch>=0.4',
         'docopt>=0.6.0,<0.7.0',
         'clint>=0.4.0,<0.6.0',
         'six>=1.0.0,<2.0.0',

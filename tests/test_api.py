ff8a6ad4b75b6a451016f4857e88b5ab25b39496
jake <jake@archive.org>
2017-09-20 16:54:28 -0700

    updated tests after changes to search.

--- tests/test_api.py
+++ tests/test_api.py
@@ -19,7 +19,7 @@ TEST_SEARCH_RESPONSE = load_test_data_file('advanced_search_response.json')
 TEST_SCRAPE_RESPONSE = load_test_data_file('scrape_response.json')
 _j = json.loads(TEST_SCRAPE_RESPONSE)
 del _j['cursor']
-_j['items'] = [{'identifier': 'nasa'}]
+_j['docs'] = [{'identifier': 'nasa'}]
 TEST_SCRAPE_RESPONSE = json.dumps(_j)
 
 
@@ -250,7 +250,7 @@ def test_search_items(session):
                  body=TEST_SCRAPE_RESPONSE,
                  match_querystring=True)
         rsps.add(responses.POST, count_url,
-                 body='{"items":[],"count":0,"total":1}',
+                 body='{"docs":[],"count":0,"total":1}',
                  match_querystring=True,
                  content_type='application/json; charset=UTF-8')
         r = search_items('identifier:nasa', archive_session=session)
@@ -265,7 +265,7 @@ def test_search_items(session):
 
 def test_search_items_with_fields(session):
     _j = json.loads(TEST_SCRAPE_RESPONSE)
-    _j['items'] = [
+    _j['docs'] = [
         {'identifier': 'nasa', 'title': 'NASA Images'}
     ]
     search_response_str = json.dumps(_j)
@@ -280,7 +280,7 @@ def test_search_items_with_fields(session):
                  match_querystring=True,
                  body=search_response_str)
         rsps.add(responses.POST, count_url,
-                 body='{"items":[],"count":0,"total":1}',
+                 body='{"docs":[],"count":0,"total":1}',
                  match_querystring=True,
                  content_type='application/json; charset=UTF-8')
         r = search_items('identifier:nasa', fields=['identifier', 'title'],
@@ -310,7 +310,7 @@ def test_page_row_specification(session):
         rsps.add_metadata_mock('nasa')
         rsps.add(responses.POST,
                  '{0}//archive.org/services/search/v1/scrape'.format(PROTOCOL),
-                 body='{"items":[],"count":0,"total":1}',
+                 body='{"docs":[],"count":0,"total":1}',
                  match_querystring=False,
                  content_type='application/json; charset=UTF-8')
         r = search_items('identifier:nasa', params={'page': '1', 'rows': '1'},

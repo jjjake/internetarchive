0cfff85f1689f4ca43b92d2a67bd89d7222a3620
jake <jake@archive.org>
2020-03-15 12:21:54 -0700

    Don't override command-line params

--- internetarchive/cli/ia_tasks.py
+++ internetarchive/cli/ia_tasks.py
@@ -111,10 +111,11 @@ def main(argv, session):
 
     if not args['<identifier>'] \
             and not params.get('task_id'):
-        params.update(dict(catalog=1, history=0))
+        _params = dict(catalog=1, history=0)
+        _params.update(params)
+        params = _params
 
     if not any(x in params for x in queryable_params):
-        # TODO: WTF
         _params = dict(submitter=session.user_email, catalog=1, history=0, summary=0)
         _params.update(params)
         params = _params

ae8e7a43f5d7a4f6e78c04d0c0ad4911d73e9af1
jake <jake@jakes-MacBook-Pro-2.local>
2024-03-19 10:47:45 -0700

    fixed test

--- tests/test_item.py
+++ tests/test_item.py
@@ -32,7 +32,7 @@ EXPECTED_S3_HEADERS = {
     'x-archive-auto-make-bucket': '1',
     'authorization': 'LOW a:b',
     'accept': '*/*',
-    'accept-encoding': 'gzip, deflate, br',
+    'accept-encoding': 'gzip, deflate',
     'connection': 'close',
 }
 
